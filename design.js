2780cc6befbcef7f831eb0f0b8ffa7f3298e1256
shedeemo <shedeed.1@osu.edu>
2023-05-31 21:40:48 -0400

    finalized most things 5-31

--- design.js
+++ design.js
@@ -1,17 +1,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const navLinks = document.querySelectorAll('nav a');
-  
-    navLinks.forEach((link) => {
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-      });
+  const navLinks = document.querySelectorAll('nav a');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+      targetSection.scrollIntoView({ behavior: 'smooth' });
     });
   });
-  
+});
 
-  function sendEmail() {
-    window.location.href = 'mailto:shedeed.1@osu.edu'
-  }
+
+function sendEmail() {
+  window.location.href = 'mailto:shedeed.1@osu.edu'
+}
+
+function sendEmail() {
+  window.location.href = 'mailto:shedeed.1@osu.edu';
+}
+
+function getHomepageURL() {
+  const currentPath = window.location.pathname;
+  const homepageURL = currentPath.substring(0, currentPath.lastIndexOf('/') + 1) + 'index.html';
+  return homepageURL;
+}
